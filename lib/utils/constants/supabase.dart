d4284f26613fe56dca7a079c9898f4015fb754e0
daniel.cortes.intern <daniel.cortes.intern@mindshore.eu>
2025-04-09 20:21:13 +0200

    Añado constantes para las credenciales de supabase para una mejor organizacion

---
+++ lib/utils/constants/supabase.dart
@@ -0,0 +1,5 @@
+class SupabaseCredential {
+  static const String url = 'https://lobqmpwqesuhavuminux.supabase.co';
+  static const String anonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvYnFtcHdxZXN1aGF2dW1pbnV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5OTIwNDYsImV4cCI6MjA1ODU2ODA0Nn0.iVrfl4GJiePUd5OAOrA6R4ZWEMJFmczGjobBa-x9Nic';
+}

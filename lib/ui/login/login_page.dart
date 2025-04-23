4a89168dd454bbfd919c13a8d2f22937d899fe48
daniel.cortes.intern <daniel.cortes.intern@mindshore.eu>
2025-04-23 14:27:33 +0200

    Añadir directivas de ignorar para mejorar la legibilidad del código

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously, must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';

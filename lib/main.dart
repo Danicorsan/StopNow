be1552dc540251a37fd5050a293d5e7f558c97c6
daniel.cortes.intern <daniel.cortes.intern@mindshore.eu>
2025-04-25 19:20:08 +0200

    Cambio initialRoute

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, __) => MaterialApp(
-        initialRoute: AppRoutes.profile,
+        initialRoute: AppRoutes.home,
         onGenerateRoute: AppRoutes.generateRoute,
         debugShowCheckedModeBanner: false,
         title: 'StopNow',
